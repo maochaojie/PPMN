ce078b11ed8a0cbc68f4b317e3f074be31d27020
chaojie mao <mcj@zju.edu.cn>
2018-03-07 16:32:34 +0800

    update

--- experiments/cuhk03/ctm_hnm/CMC_test/read_mat.py
+++
@@ -1,9 +0,0 @@
-import scipy.io as sio
-import sys
-mat_file = sys.argv[1]
-a=sio.loadmat(mat_file)
-b = a['ours']
-for iter in range(2000,16000,1000):
-	key = 'single_yaqing_%d'%iter
-	c = b[key]
-	print '%f %s'%(c[0][0][0][0],key)
